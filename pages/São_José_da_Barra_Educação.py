cedf9f5007ef75df26747383ce34c9c4bea531eb
TTbrazilian <tulioteixeira2005@gmail.com>
2026-04-15 02:49:33 -0300

    UPDATE

--- pages/São_José_da_Barra_Educação.py
+++ pages/São_José_da_Barra_Educação.py
@@ -126,9 +126,9 @@ def buscar_arquivo(nome):
 
 @st.cache_data
 def load_all_data():
-    arquivo_f = "zEducação/São_José_da_Barra.csv"
-    arquivo_r = "zEducação/São_José_da_Barra_R.csv"
-    arquivo_df = "zEducação/São_José_da_Barra_DF.csv"
+    arquivo_f = "zEducação/São José da Barra.csv"
+    arquivo_r = "zEducação/São José da Barra_R.csv"
+    arquivo_df = "zEducação/São José da Barra_DF.csv"
     
     path_f = buscar_arquivo(arquivo_f)
     path_r = buscar_arquivo(arquivo_r)
