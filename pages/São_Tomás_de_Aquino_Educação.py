bba434a94dda70bea837d112f2a45aa670e7d636
TTbrazilian <tulioteixeira2005@gmail.com>
2026-04-29 15:30:56 -0300

    UPDATE

--- pages/São_Tomás_de_Aquino_Educação.py
+++ pages/São_Tomás_de_Aquino_Educação.py
@@ -436,7 +436,7 @@ if df_f_raw is not None and df_r is not None:
             fig_70.update_layout(
                 separators=",.", barmode='group', hoverlabel=HOVER_STYLE,
                 yaxis=dict(showticklabels=False), showlegend=True,
-                legend=dict(orientation="h", yanchor="bottom", y=-0.20,
+                legend=dict(orientation="h", yanchor="bottom", y=-0.25,
                             xanchor="center", x=0.5), height=420
             )
             st.plotly_chart(fig_70, use_container_width=True, config=CONFIG_PT)
